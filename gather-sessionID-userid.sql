c641c3879e98754c1c89389dc29a890cfb4c7008
Sachin GTM <sachin.spinx@gmail.com>
2024-10-14 19:07:29 +0530

    Update gather-sessionID-userid.sql

--- gather-sessionID-userid.sql
+++ gather-sessionID-userid.sql
@@ -61,3 +61,26 @@ GROUP BY
   event_date,
   page_url
 order by event_date desc
+
+
+--------------------------------------------------
+
+SELECT
+  PARSE_DATE('%Y%m%d', event_date) AS event_date,
+  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = "page_location") AS page_url,
+  COUNT(DISTINCT CONCAT(
+    (SELECT value.int_value FROM UNNEST(event_params) WHERE key = "ga_session_id"),
+    "_",
+    user_pseudo_id
+  )) AS sessions
+FROM
+  `calendow.analytics_340229049.events_*`
+WHERE
+  event_name = 'page_view'  -- Filter for page view events
+  AND PARSE_DATE('%Y%m%d', event_date) BETWEEN '2024-09-01' AND '2024-09-30'
+GROUP BY
+  event_date,
+  page_url
+ORDER BY
+  event_date,
+  sessions DESC
